6089acee49d5883128c95a550b19fddcfd26ac86
buntydkd <buntydkd77@gmail.com>
2024-03-01 23:31:45 +0530

    commit to module

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants";
+// src/index.js
+import kingDB from "./db/index.js";
+
+dotenv.config({
+    path: `/env`
+});
+
+kingDB();
+
+
+//method 1 to connect db
+// import { Express } from "express";
+// const app = express()
+
+// ( async() => {
+//     try{
+//        await mongoose.connect('${process.env.MONGODB_URI}/{DB_NAME}')
+//        app.on("error",(error)=>{
+//         console.log("errr:",error);
+//         throw error
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log('APP is listening on port ${process.env.PORT}');
+//        })
+//     }catch(error){
+//         console.log("ERROR",error)
+//         throw err
+//     }
+// })()
